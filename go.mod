d2c49283a148a29240e1f6774af7a9a30a087767
PolovinaD <dule031ue@gmail.com>
2022-02-02 18:07:22 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github-cve
+
+go 1.17
+
+require (
+	github.com/shurcooL/githubv4 v0.0.0-20220115235240-a14260e6f8a2
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
+)
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/shurcooL/graphql v0.0.0-20200928012149-18c5c3165e3a // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"golang.org/x/oauth2"
+	"io/ioutil"
+	"strings"
+	"time"
+
+	"github.com/shurcooL/githubv4"
+)
+
+type Repository struct {
+	Url         string
+	Description string
+}
+
+var CVEQuery struct {
+	Search struct {
+		RepositoryCount int
+		PageInfo        struct {
+			EndCursor   githubv4.String
+			StartCursor githubv4.String
+		}
+		Edges []struct {
+			Node struct {
+				Repo Repository `graphql:"... on Repository"`
+			}
+		}
+	} `graphql:"search(query: $query, type: REPOSITORY, first: 100)"`
+}
+
+var CVEPaginationQuery struct {
+	Search struct {
+		RepositoryCount int
+		PageInfo        struct {
+			EndCursor   githubv4.String
+			StartCursor githubv4.String
+		}
+		Edges []struct {
+			Node struct {
+				Repo Repository `graphql:"... on Repository"`
+			}
+		}
+	} `graphql:"search(query: $query, type: REPOSITORY, first: 100, after: $after)"`
+}
+
+func main() {
+	token := flag.String("token", "", "Github token")
+	query := flag.String("query", "", "Query")
+	flag.Parse()
+
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: *token},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+
+	client := githubv4.NewClient(httpClient)
+
+	repos := make([]Repository, 0)
+	variables := map[string]interface{}{
+		"query": githubv4.String(*query),
+	}
+
+	err := client.Query(context.Background(), &CVEQuery, variables)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, nodeStruct := range CVEQuery.Search.Edges {
+		repos = append(repos, nodeStruct.Node.Repo)
+	}
+
+	maxRepos := CVEQuery.Search.RepositoryCount
+	reposCnt := len(repos)
+
+	variables = map[string]interface{}{
+		"query": githubv4.String(*query),
+		"after": CVEQuery.Search.PageInfo.EndCursor,
+	}
+	for reposCnt < maxRepos {
+		time.Sleep(time.Second)
+
+		err = client.Query(context.Background(), &CVEPaginationQuery, variables)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if len(CVEPaginationQuery.Search.Edges) == 0 {
+			break
+		}
+		for _, nodeStruct := range CVEPaginationQuery.Search.Edges {
+			repos = append(repos, nodeStruct.Node.Repo)
+		}
+
+		reposCnt = len(repos)
+		variables["after"] = CVEPaginationQuery.Search.PageInfo.EndCursor
+	}
+
+	data, _ := json.MarshalIndent(repos, "", "   ")
+
+	err = ioutil.WriteFile(strings.Trim(*query, "-*")+".json", data, 0644)
+	if err != nil {
+		fmt.Println("Couldn't save data into a file!")
+	}
+
+}
